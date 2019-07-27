89af44d6f1ce8d27e80a9a242f64a4934bcca314
husxwy <260323310@qq.com>
2019-07-27 21:01:33 +0800

    Create minikube-v1.2.0.sh

---
+++ minikube-v1.2.0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#minikube version: v1.2.0
+
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/kube-proxy:v1.15.0
+#k8s.gcr.io/kube-proxy:v1.15.0
+
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/kube-apiserver:v1.15.0
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/kube-controller-manager:v1.15.0
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/kube-scheduler:v1.15.0
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/kube-addon-manager:v9.0
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/coredns:1.3.1
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.1
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/etcd:3.3.10
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/pause:3.1
+#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+#k8s.gcr.io/echoserver:1.4
