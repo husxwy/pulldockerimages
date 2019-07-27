60a93b705a2fd06eec559200dd2bdc37c6e23f45
husx <husxwy@163.com>
2019-07-27 21:08:11 +0800

    添加aliyun

--- minikube-v1.2.0.sh
+++ minikube-v1.2.0.sh
@@ -6,24 +6,24 @@ tee << EOF > /etc/docker/daemon.json
   "dns": ["10.0.2.3","223.5.5.5"]
 }
 EOF
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/kube-proxy:v1.15.0
-#k8s.gcr.io/kube-proxy:v1.15.0
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/kube-proxy:v1.15.0
+# k8s.gcr.io/kube-proxy:v1.15.0
 
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/kube-apiserver:v1.15.0
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/kube-controller-manager:v1.15.0
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/kube-scheduler:v1.15.0
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/kube-addon-manager:v9.0
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/coredns:1.3.1
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.1
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/etcd:3.3.10
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/pause:3.1
-#docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
-#k8s.gcr.io/echoserver:1.4
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/kube-apiserver:v1.15.0
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/kube-controller-manager:v1.15.0
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/kube-scheduler:v1.15.0
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/kube-addon-manager:v9.0
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/coredns:1.3.1
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.1
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/etcd:3.3.10
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/pause:3.1
+# docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/
+# k8s.gcr.io/echoserver:1.4
